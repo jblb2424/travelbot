685f4cb27a89d0e688deec3c66ec8fe03cd654d9
Justin Barry <justin.barry97@gmail.com>
2019-12-29 17:10:50 -0500

    h

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
 
 
 schedule.every().day.at("09:00").do(main)
-schedule.every().day.at("21:20").do(main)
+schedule.every().day.at("22:13").do(main)
 schedule.every().day.at("18:00").do(main)
 
 while True:
