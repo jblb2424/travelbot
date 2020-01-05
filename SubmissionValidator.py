1d9eb98c85065a4b4444fdcfa91ffa3840f15e42
Justin Barry <justin.barry97@gmail.com>
2020-01-05 14:12:05 -0500

    fix aspect ratios

--- SubmissionValidator.py
+++ SubmissionValidator.py
@@ -2,11 +2,14 @@ from PIL import Image
 #Class encapsulating set of rules a submission must pass in order to be posted
 class SubmissionValidator:
 	def __init__(self):
-		self.valid_aspect_ratios = [1, 1.25, 1.77]
+		self.valid_aspect_ratios = [1]
 
 	def is_correct_image_size(self, image):
 		width, height = Image.open(image).size
 		aspect_ratio = width / height
+		print(aspect_ratio)
+		print(width)
+		print(height)
 		for valid_ratio in self.valid_aspect_ratios:
 			if abs(aspect_ratio - valid_ratio) < .2: return True
 		return False
